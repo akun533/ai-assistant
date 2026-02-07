c873d09fe4a3aa5b888e24ec64e4d2737b273b2b
akun533 <122915623@qq.com>
2026-02-08 06:44:32 +0800

    model: add

--- src/service/ocr.ts
+++ src/service/ocr.ts
@@ -8,7 +8,7 @@ import path from 'path';
 
 // 获取项目根目录（绝对路径）
 const PROJECT_ROOT = process.cwd();
-const MODELS_DIR = path.join(PROJECT_ROOT, 'models');
+const MODELS_DIR = path.join(PROJECT_ROOT, 'src/service/models');
 
 // 模型文件路径
 const DET_MODEL = path.join(MODELS_DIR, 'PP-OCRv5_mobile_det_infer.onnx');
